601aeb0479cb23e7cbe7f1525c7332bb0284cdd8
lamack <1317488806@qq.com>
2017-06-16 18:23:11 +0800

    3d

---
+++ index.js
@@ -0,0 +1,20 @@
+var startX,startY;
+$(window).on('mousedown',function(e){
+	e.preventDefault();
+	startX = e.clientX;
+	startY = e.clientY;
+
+	$(window).on('mousemove',function(e){
+		moveX = e.clientX;
+		moveY = e.clientY
+		var difX = moveX-startX;
+		var difY = moveY- startY;
+		var cla =  $('.wrapper').attr('class')
+		$('.wrapper').css({transform:'rotateX('+(-difY)+'deg) rotateY('+difX+'deg) rotateZ(0deg)'})
+	})
+})
+$(window).on('mouseup',function(e){
+	$(window).off('mousemove')
+	var tran = $('.wrapper').get(0).style.transform;
+		
+})
